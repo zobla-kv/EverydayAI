e4a6f462f643d59c950b5663a201f7e28582dc54
zobla <zobla.kv@gmail.com>
2023-01-31 23:14:00 +0100

    cleanup - remove spinner component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,6 @@
 export * from '../app.component';
 export * from './header/header.component';
 export * from './footer/footer.component';
-export * from './spinner/spinner.component';
 export * from './home-page/home-page.component';
 export * from './home-page/category-selector/category-selector.component';
 export * from './product-page/product-page.component';
