05335324f6f0dabe8f05f7a3cd5073e5bd3cc066
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-02-01 21:17:07 +0100

    add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,22 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-/* firebase */
+// *************  firebase ******************* //
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import * as firebaseConfig from '../../firebase.json';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+const firebaseConfig = {
+  apiKey: "AIzaSyBdBGoR3VvzO59rVahX9oGU5XKNLwAAF2Y",
+  authDomain: "house-of-dogs-11bfb.firebaseapp.com",
+  projectId: "house-of-dogs-11bfb",
+  storageBucket: "house-of-dogs-11bfb.appspot.com",
+  messagingSenderId: "829869000499",
+  appId: "1:829869000499:web:6669cdd8ebf78446ee6046",
+  measurementId: "G-66BZM3SHPN"
+}
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import * as firebaseConfig from '../../firebase.json';
+
+// ****************************************** //
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -57,8 +70,9 @@ import {
 
     // firebase
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: firebaseConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
