7bbad57271e3cefb90c7f3f44c7d8f7a56ac1987
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-02-07 22:51:52 +0100

    split app between client and backend

---
+++ express/app.js
@@ -0,0 +1,5 @@
+const emailService = require('./emailService');
+
+// TODO: trigger on rest
+emailService.sendEmail('houseofdogs.online@gmail.com', 'zobla', 'activation');
+
---
+++ express/emailService.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports.sendEmail = async function (email, user, type) {
+  type = type.toLowerCase();
+  if(type !== 'activation' && type !== 'reset password') {
+    throw new Error('Invalid email functionality');
+  }
+  
+//   const token = jwt.sign({ user }, process.env.JWT_SECRET);
+  const token = 'test-test-test-test';
+
+  const host = 'https://www.house-of-dogs.com'
+
+  // capitalize first letter
+  const subject = `${type[0].toUpperCase() + type.slice(1)} 🐕`; 
+
+  type = type.replace(/ /g, '_');
+
+  const message = 
+  `
+    <h2 style="margin-left: 250px;color:black;">
+      Verify your email by clicking link below
+    </h2>
+    <a href="${host}/${type}/${token}">
+      ${host}/${type}/${token}
+    </a>
+  `;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_AUTH_USERNAME,
+      pass: process.env.EMAIL_AUTH_PASSWORD,
+    },
+  });
+  await transporter.sendMail({
+    from: '"House of dogs" <houseofdogs.online@gmail.com>',
+    to: email,
+    subject,
+    html: message,
+  });
+};
