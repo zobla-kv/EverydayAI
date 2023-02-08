7c5525cd5fd7d5f1bb50ae855888b435f763e83c
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-02-08 16:20:13 +0100

    remove cors and body parser dependencies

--- express/app.js
+++ express/app.js
@@ -1,19 +1,16 @@
 // app
 const express = require('express');
 const app = express();
-// TODO: remove later if not needed
-// remove body parser also
-const cors = require('cors');
 
-// cors validator
-const corsValidator = require('./cors');
+// cors 
+const cors = require('./cors');
 
 // endpoints
 const routes = require('./routes');
 
 // app settings
 app.use(express.json());
-app.use('/api', corsValidator, routes);
+app.use('/api', cors, routes);
 
 app.listen(3000, () => {
   console.log('server started on port 3000');
--- express/cors.js
+++ express/cors.js
@@ -1,7 +1,7 @@
 // TODO: change localhost for prod
-module.exports = function corsValidator(req, res, next) {
+module.exports = function cors(req, res, next) {
   const origin = req.header('origin');
-  if (origin.includes('localhost')) {
+  if (origin && origin.includes('localhost')) {
     res.set('Access-Control-Allow-Origin', origin);
     res.set('Access-Control-Allow-Headers', 'Content-type');
     res.set('Access-Control-Allow-Methods', 'GET, POST');
