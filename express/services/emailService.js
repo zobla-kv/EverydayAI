518dcb3f14b78886150e6d23632849191fe069e8
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-02-13 15:22:01 +0100

    add logo to email

--- express/services/emailService.js
+++ express/services/emailService.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer');
 const firebaseService = require('./firebaseService');
+const path = require('path');
 
-module.exports.sendEmail = async function (email, type) {
+module.exports = async function (email, type) {
   type = type.toLowerCase();
   if (type !== 'activation' && type !== 'reset password') {
     return false;
@@ -20,7 +21,8 @@ module.exports.sendEmail = async function (email, type) {
   type = type.replace(/ /g, '_');
 
   const message =
-  `
+  ` 
+    <img src="cid:logo">
     <h2 style="margin-left: 250px;color:black;">
       Verify your email by clicking link below
     </h2>
@@ -31,6 +33,7 @@ module.exports.sendEmail = async function (email, type) {
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_AUTH_USERNAME,
       pass: process.env.EMAIL_AUTH_PASSWORD,
@@ -43,6 +46,11 @@ module.exports.sendEmail = async function (email, type) {
     to: email,
     subject,
     html: message,
+    attachments: [{
+      filename: 'main-logo.png',
+      path: path.join(__dirname, '..', '/assets/main-logo.png'),
+      cid: 'logo'
+  }]
   })
   .then(() => isSent = true);
 
