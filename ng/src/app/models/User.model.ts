58b83090c425881b5170e2cfb489cd91273be4d0
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-03-23 20:56:57 +0100

    change user model to include shopping cart (start refactor user model)

--- ng/src/app/models/User.model.ts
+++ ng/src/app/models/User.model.ts
@@ -1,6 +1,10 @@
 // TODO: distinguish between Firebase user (User from 2 lines below, not class name) 
 // and Custom user stored in firestore db (RegisterUser)
 // import { User as FirebaseUser } from '@angular/fire/auth
+import { 
+  ShoppingCart
+} from '@app/models';
+
 export class User {
   constructor(
     public id: string,
@@ -22,4 +26,17 @@ export interface RegisterUser {
   email: string,
   password: string
   gender: string
+}
+
+// custom user stored in db that is different from frebase user
+// TODO: try implementing auto logout (token into this user)
+// TODO: eliminate password from here
+export class CustomUser {
+  id: string;
+  email: string;
+  name: string;
+  gender: string;
+  cart: ShoppingCart;
+  // used for registration and login
+  password?: string;
 }
