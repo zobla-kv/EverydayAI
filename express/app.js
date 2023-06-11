83019042e86139b2c0e7ef764dbcd17a16f7348a
zobla <zobla.kv@gmail.com>
2023-06-11 22:23:26 +0200

    add config for env variables

--- express/app.js
+++ express/app.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv').config({
+  path: path.join(__dirname, '.env') }
+);
 
 const PORT = process.env.PORT || 3030;
 
