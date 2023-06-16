f940716ba64465d2268735468a7c440d61ec2d4a
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-06-16 15:31:42 +0200

    use http server

--- express/app.js
+++ express/app.js
@@ -1,15 +1,16 @@
 // app
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
 // require('dotenv').config({
 //   path: path.join(__dirname, '.env') }
 // );
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 const currentDir = path.join(__dirname);
 
@@ -24,6 +25,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', cors, routes);
 
+
 // '*' for angular routing to work
 app.get('*', (req, res) => {
   console.log('got request');
@@ -32,15 +34,16 @@ app.get('*', (req, res) => {
 })
 
 // server
-const sslServer = https.createServer({
-  // NOTE: cert folders unused in production
-  key: process.env.CERT_KEY.replace(/\\n/g, '\n'),
-  cert: process.env.CERT.replace(/\\n/g, '\n'),
-}, app);
+// const sslServer = https.createServer({
+//   // NOTE: cert folders unused in production
+//   key: process.env.CERT_KEY.replace(/\\n/g, '\n'),
+//   cert: process.env.CERT.replace(/\\n/g, '\n'),
+// }, app);
 
 
 console.log('starting server...');
+console.log('HTTP SERVER')
 
-sslServer.listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 })
