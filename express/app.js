3fb54d3f598c788fde13fb28e1be8f8a8a232f9e
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-06-17 10:14:41 +0200

    add dirname to express.static

--- express/app.js
+++ express/app.js
@@ -21,7 +21,7 @@ const cors = require('./cors');
 const routes = require('./routes');
 
 // app settings
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use('/api', cors, routes);
 
