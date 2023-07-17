eaf82ccecb1f396f1718abf1140f963a2f0f7cd5
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-07-17 12:33:51 +0200

    updage change page animation in product list. Set height of top section of product

--- ng/src/app/components/product-page/product-list/product-list.animations.ts
+++ ng/src/app/components/product-page/product-list/product-list.animations.ts
@@ -29,27 +29,27 @@ export default [
   trigger('changePage', [
     state('show', style({
       'opacity': '1',
-      'transform': 'translateX(0px)'
+      'transform': 'translateY(0px)'
     })),
     state('hideLeft', style({
       'opacity': '0',
-      'transform': 'translateX(20px)'
+      'transform': 'translateY(20px)'
     })),
     state('hideRight', style({
       'opacity': '0',
-      'transform': 'translateX(-20px)'
+      'transform': 'translateY(-20px)'
     })),
     transition('show => hideLeft', animate('750ms ease-in-out', keyframes([
-      style({ 'opacity': '1', 'transform': 'translateX(-5px)', offset: 0.25 }),
-      style({ 'opacity': '1', 'transform': 'translateX(-15px)', offset: 0.5 }),
-      style({ 'opacity': '0.2', 'transform': 'translateX(-40px)', offset: 0.9 }),
-      style({ 'opacity': '0', 'transform': 'translateX(-50px)', offset: 1 }),
+      style({ 'opacity': '1', 'transform': 'translateY(-5px)', offset: 0.25 }),
+      style({ 'opacity': '1', 'transform': 'translateY(-15px)', offset: 0.5 }),
+      style({ 'opacity': '0.2', 'transform': 'translateY(-40px)', offset: 0.9 }),
+      style({ 'opacity': '0', 'transform': 'translateY(-50px)', offset: 1 }),
     ]))),
     transition('show => hideRight', animate('750ms ease-in-out', keyframes([
-      style({ 'opacity': '1', 'transform': 'translateX(5px)', offset: 0.25 }),
-      style({ 'opacity': '1', 'transform': 'translateX(15px)', offset: 0.5 }),
-      style({ 'opacity': '0.2', 'transform': 'translateX(40px)', offset: 0.9 }),
-      style({ 'opacity': '0', 'transform': 'translateX(50px)', offset: 1 }),
+      style({ 'opacity': '1', 'transform': 'translateY(5px)', offset: 0.25 }),
+      style({ 'opacity': '1', 'transform': 'translateY(15px)', offset: 0.5 }),
+      style({ 'opacity': '0.2', 'transform': 'translateY(40px)', offset: 0.9 }),
+      style({ 'opacity': '0', 'transform': 'translateY(50px)', offset: 1 }),
     ]))),
     transition('hideLeft => show', animate(300)),
     transition('hideRight => show', animate(300)),
