5fdd198b6a7fd8a9a5e1df5710513c84407f3f01
Nemanja Blagojevic <nemanja.blagojevic@hpe.com>
2023-07-23 13:17:40 +0200

    remove bug comment

--- ng/src/app/components/product-page/product-list/product-list.component.ts
+++ ng/src/app/components/product-page/product-list/product-list.component.ts
@@ -115,7 +115,6 @@ export class ProductListComponent implements OnInit {
 
   // updates page number in pagination
   updatePageNumber() {
-    // TODO: bug, page 1/0
     const list = document.getElementsByClassName('mat-mdc-paginator-range-label');
     list[0] && (list[0].innerHTML = 'Page: ' + (this.paginator.pageIndex + 1) + '/' + (this.paginator.getNumberOfPages() + 1));
   }
