055f779057d2bf62979a2f9e27d2557dd340cabe
zobla <zobla.kv@gmail.com>
2023-11-28 22:06:02 +0100

    add back origin variable

--- express/cors.js
+++ express/cors.js
@@ -1,6 +1,7 @@
 // require('cors') TODO: there is cors package (check it's code)
 
 module.exports = function cors(req, res, next) {
+  const origin = req.header('origin');
   console.log('origin: ', origin);
   if (origin && origin.includes(process.env.HOST_URL)) {
     res.set('Access-Control-Allow-Origin', origin);
