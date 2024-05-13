5b902287fe13a8372117fbb0e8e5a862623bedf1
zobla <zobla.kv@gmail.com>
2024-05-13 12:49:10 +0200

    revert previous commit

--- ng/src/environments/environment.prod.ts
+++ ng/src/environments/environment.prod.ts
@@ -10,6 +10,5 @@ export default {
     appId: "1:457726492799:web:efbb37d1c806d974fa3f4a",
     measurementId: "G-E8J9BMHPJD"
   },
-  // paypal_client_id: 'Ab33ADZA1jVVQVDTJ8AuQ__ZYFI_CYJ45bw_UHB3nJVijkvVRk5mrDzRLSneCdDz4nu2JVZ3tp_qlKAZ'
-  paypal_client_id: 'AZ8-JP1AhDSz4dHmfpPOE51qQKnES7iwVraXgfR6jC3PLrjmXIEp3RiMjQtunqHD_uGwpvCTTf251YXT'
+  paypal_client_id: 'Ab33ADZA1jVVQVDTJ8AuQ__ZYFI_CYJ45bw_UHB3nJVijkvVRk5mrDzRLSneCdDz4nu2JVZ3tp_qlKAZ'
 };
